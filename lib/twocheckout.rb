a006bc4dd50e815a3550a6b58631c908124ec774
Ernesto Garcia <ernesto+git@gnapse.com>
2012-08-22 23:33:04 -0400

    Add Company class to wrap API calls that retrieve vendor info

--- lib/twocheckout.rb
+++ lib/twocheckout.rb
@@ -4,6 +4,7 @@ require "twocheckout/hash_object"
 
 # Wrappers of conceptual objects obtained from 2Checkout responses
 require "twocheckout/purchase_result"
+require "twocheckout/company"
 require "twocheckout/lineitem"
 require "twocheckout/invoice"
 require "twocheckout/sale"
---
+++ lib/twocheckout/company.rb
@@ -0,0 +1,27 @@
+require 'singleton'
+
+module Twocheckout
+  class Company < HashObject
+    include Singleton
+
+    def initialize
+      response = Twocheckout::API.request(:get, 'acct/detail_company_info')
+      super(response['vendor_company_info'])
+    end
+
+    def contact_info
+      if @contact_info.nil?
+        response = Twocheckout::API.request(:get, 'acct/detail_contact_info')
+        @contact_info = HashObject.new(response['vendor_contact_info'])
+      end
+      @contact_info
+    end
+
+    protected
+
+    def _key
+      self.vendor_id
+    end
+
+  end
+end
